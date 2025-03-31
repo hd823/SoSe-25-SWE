a6840d25f504a6b09c61b8d65c25827d0dbcdcb8
dh823 <dh8239@mci4me.at>
2025-03-31 21:10:27 +0200

    main.py mit meinem date übernommen

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from datetime import datetime
-from my_functions import estimate_max_hr, build_person, build_experiment
-
+from my_functions import estimate_max_hr, build_person, build_experiment, ask_name, ask_number, ask_sex
 
 if __name__ == "__main__":
-    try:
-        experiment_name = input("Bitte geben Sie den Namen des Experiments ein: ")
-        date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-        supervisor = input("Bitte geben Sie den Namen des Betreuers ein: ")
-        first_name = input("Bitte geben Sie den Vornamen des Probanden ein: ")
-        last_name = input("Bitte geben Sie den Nachnamen des Probanden ein: ")
-        sex = input("Bitte geben Sie das Geschlecht des Probanden ein: ")
-        age = int(input("Bitte geben Sie das Alter des Probanden ein: "))
-        experiment = build_experiment(experiment_name, date, supervisor, build_person(first_name, last_name, sex, age))
-        print(experiment)
-    except ValueError:
-        print("Fehler mit der Eingabe.")
+    date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Erstellen eines Leistungstests
+    print("Leistungstest wird erstellt")
+    print("Bitte geben Sie die Diagnostikerdaten ein:")
+
+    print(" Geben Sie den Vornamen des Diagnostikers ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Diagnostikers ein:")    
+    last_name = ask_name()
+
+    supervisor = build_person(first_name, last_name, None, None)
+
+    print("Bitte geben Sie die Probandendaten ein:")
+    print(" Geben Sie den Vornamen des Probanden ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Probanden ein:")
+    last_name = ask_name()
+    print(" Geben Sie das Alter des Probanden ein:")
+    age = ask_number()
+    sex = ask_sex()
+    # Erstellen einer Person
+    subject = build_person(first_name, last_name, sex, age)
+
+    # Erstellen eines Experiments
+    experiment = build_experiment("Leistungstest", date, supervisor, subject)
+
+    print("Das Experiment wurde erstellt:")
+    print(experiment)
