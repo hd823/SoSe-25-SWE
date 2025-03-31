84aa169c7ab4b9363f81618e17c97f6fc69c5d67
dh823 <dh8239@mci4me.at>
2025-03-31 21:06:50 +0200

    my functions aktualisiert und .gitignore angefangen

--- my_functions.py
+++ my_functions.py
@@ -31,3 +31,32 @@ def build_experiment(experiment_name, date, supervisor, subject) -> dict:
             "subject" :   subject
             }
     return experiment_dict
+
+def ask_name() -> str:
+    """Asks the user for their name."""
+    try :
+        name = input("What is your name? ")
+        return name
+    except Exception as e:
+        print("Please enter a valid name")
+        ask_name()
+
+def ask_number() -> int:
+    """Asks the user for a number."""
+    try:
+        number = int(input("Enter a number: "))
+        return number
+    except Exception as e:
+        print("Please enter a valid number")
+        ask_number()
+
+def ask_sex() -> str:
+    """Ask the user for the sex"""
+    sex_string = input("Enter sex (w/m): ")
+    if sex_string == "w":
+        return "female"
+    elif sex_string == "m":
+        return "male"
+    else:
+        print("Please enter 'w' or 'm'")
+        ask_sex()
